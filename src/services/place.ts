ad49e8d6fbce613edc9dd39b32fa991b5c5cc2a9
eunchae <kec0130@gmail.com>
2024-02-29 00:44:18 +0900

    Delete all files

--- src/services/place.ts
+++
@@ -1,9 +0,0 @@
-import { IPlaceApiRes } from 'types/place'
-
-const PLACE_BASE_URL = 'https://www.chaechae.site'
-
-export const getPlaceInferenceApi = (formData: FormData): Promise<IPlaceApiRes> =>
-  fetch(`${PLACE_BASE_URL}/api/places/inference/`, {
-    method: 'POST',
-    body: formData,
-  }).then((res) => res.json())
--- src/types/place.ts
+++
@@ -1,6 +0,0 @@
-export interface IPlaceApiRes {
-  label_category: string
-  sentence: string
-}
-
-export type TSearchStatus = 'init' | 'loading' | 'done' | 'error'
